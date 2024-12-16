422e9a19a546d895f0b554fab892165efd9e07c3
Lee Min Woo <lmw7414@naver.com>
2024-12-16 15:20:53 +0900

    delivery-service: init project

---
+++ DeliverySerive/src/main/java/org/example/deliveryserive/DeliverySeriveApplication.java
@@ -0,0 +1,13 @@
+package org.example.deliveryserive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverySeriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliverySeriveApplication.class, args);
+    }
+
+}
