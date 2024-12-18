d08997ddf20f6638224469bd03822168fbf4f351
Lee Min Woo <lmw7414@naver.com>
2024-12-18 16:27:17 +0900

    order-service: 주문 로직 설계
    주문 로직에서는 결재, 배송, 카탈로그 관리 서비스와의 연동이 필요하다. 다음 작업에서는 도커 컴포즈로 모든 서비스를 띄워 통합 테스트를 해볼 예정

---
+++ OrderService/src/main/java/org/example/orderservice/entity/ProductOrder.java
@@ -0,0 +1,33 @@
+package org.example.orderservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+import org.example.orderservice.enums.OrderStatus;
+
+@Entity
+@NoArgsConstructor
+public class ProductOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public Long userId;
+    public Long productId; // 단일 사움에 대해서만 주문 가능하다고 가정
+    public Long count;
+    public OrderStatus orderStatus;
+    public Long paymentId;
+    public Long deliveryId;
+
+
+    public ProductOrder(Long userId, Long productId, Long count, OrderStatus orderStatus, Long paymentId, Long deliveryId) {
+        this.userId = userId;
+        this.productId = productId;
+        this.count = count;
+        this.orderStatus = orderStatus;
+        this.paymentId = paymentId;
+        this.deliveryId = deliveryId;
+    }
+}
